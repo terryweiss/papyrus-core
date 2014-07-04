e869f982aa5355d0ecb91a73cf4178bfd5baa0b7
terryweiss <terry.weiss@gmail.com>
2014-07-04 19:01:58 -0400

    ready for release

---
+++ gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.initConfig( {
+
+		shell : {
+			release : {
+				command : [
+					"git add .",
+					'git commit -m "ready for release"',
+					"npm version patch",
+					"git push",
+					"git push --tags",
+					"npm publish"
+				].join("&&")
+			}
+		}
+
+	} );
+};
